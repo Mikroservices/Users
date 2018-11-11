6cf0ddc52bca3bee7af5374591e86e777f4c7825
Marcin Czachurski <mczachurski@icloud.com>
2018-11-11 21:43:13 +0100

    Add CORS configuration

--- Sources/App/Setup/configure.swift
+++ Sources/App/Setup/configure.swift
@@ -27,6 +27,15 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 private func registerMiddlewares(services: inout Services) {
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+    middlewares.use(corsMiddleware)
+
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 }
